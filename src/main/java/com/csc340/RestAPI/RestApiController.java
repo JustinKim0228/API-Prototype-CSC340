a8223affc075cca04ede6d33f020ee0b071a0714
JustinKim0228 <97651490+JustinKim0228@users.noreply.github.com>
2022-09-14 21:45:23 -0400

    Update RestApiController.java

--- src/main/java/com/csc340/RestAPI/RestApiController.java
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -1,79 +1,39 @@
 package com.csc340.RestAPI;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 
 /**
  *
- * @author sentini
+ * @author j_kim10@uncg.edu
  */
 @RestController
 public class RestApiController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello, %s!", name);
+    private Map<String, Countries> countryMap = new HashMap<>(); 
+
+    private static RestApiController instance = new RestApiController();
+    public static RestApiController getInstance() {
+        return instance;
     }
 
-    /**
-     * Get a random quote from quotable and make it available at this endpoint.
-     *
-     * @return
-     */
-    @GetMapping("/quote")
-    public Object getQuote() {
-        String url = "https://api.quotable.io/random";
-        RestTemplate restTemplate = new RestTemplate();
-        Object jSonQuote = restTemplate.getForObject(url, Object.class);
+    private RestApiController() {
+        countryMap.put("Afghanistan", new Countries("Afghanistan", "AF"));
+        countryMap.put("Australia", new Countries("Australia", "AU"));
+        countryMap.put("United States", new Countries("United States", "US"));
+        countryMap.put("Japan", new Countries("Japan", "JP"));
+        countryMap.put("Greece", new Countries("Greece", "GR"));
+        countryMap.put("France", new Countries("France", "FR"));
 
-        //Print the whole response to console.
-        String quote = restTemplate.getForObject(url, String.class);
-        //Parse out the most important info from the response.
-        JSONObject jo = new JSONObject(quote);
-        System.out.println(jo.toString());
-        String quoteAuthor = jo.getString("author");
-        String quoteContent = jo.getString("content");
-        System.out.println(quoteAuthor);
-        System.out.println(quoteContent);
-
-        return jSonQuote;
     }
+    public Countries getCountry(String country) {
+		return countryMap.get(country);
+	}
 
-    /**
-     * Get a list of universities from hipolabs and parse the most important
-     * info to console.
-     *
-     * @return
-     */
-    @GetMapping("/universities")
-    public List<Object> getUniversities() {
-        String url = "http://universities.hipolabs.com/search?name=sports";
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] universities = restTemplate.getForObject(url, Object[].class);
+	public void putCountry(Countries country) {
+		countryMap.put(country.getCountry(), country);
 
-        //Print the whole response to console
-        JSONArray ja = new JSONArray(universities);
-        System.out.println(ja.toString());
-
-        //Parse out relevant info from each entry in the response
-        Iterator<Object> iter = ja.iterator();
-        while (iter.hasNext()) {
-            JSONObject jo = (JSONObject) iter.next();
-            String name = jo.getString("name");
-            String country = jo.getString("country");
-            System.out.println(name + " : " + country);
-
-        }
-
-        return Arrays.asList(universities);
     }
-
 }
